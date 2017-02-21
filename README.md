ea7ff6c3e5a75ed08a5334cb290957a6a92cfcec
Markus Flögel - TELEPORT <markus.floegel@teleport.de>
2017-02-21 14:27:23 +0100

    cards

--- README.md
+++ README.md
@@ -1,43 +1,15 @@
-# Build a bot 
+### Overview
 
-## Set up project
+This Bot demonstrates basic bot setup, beginning and ending dialogs, the use of attachments and a confirmation prompt.
+* It's part of the bot-education labs.
 
-### If you have a package.json
+### Exercise
 
-Create a folder for your bot, cd into it, copy all code over and run npm install to install all things in package.json.  So, in terminal type:
+1.  Setup the project according to the instructions [here](bot-hello/README.md) and note there is a package.json.
+2.  Run bot in Emulator locally to ensure it works.
+3.  Fill in the missing pieces in server.js.
+4.  Try this out in the Emulator with a test dialog ("hi" is a good start).
 
-    npm install
+### Check your work
 
-
-### If there is no package.json
-
-Create a folder for your bot, cd into it, and run npm init to begin a project.  So, in terminal type:
-
-    npm init
-    
-Get the BotBuilder (use latest when saving botbuilder below, otherwise just `3.5.4`) and Restify modules using npm.
-
-    npm install --save botbuilder@3.5.4
-    npm install --save restify
-
-## Test Locally
-
-Back in terminal:
-
-    node server.js
-
-In the botemulator desktop app:
-
-* Select app on port (usually 3978)
-* Keep app id and app password blank
-* Hit connect
-* Type a message to the bot and wait for a response (you could start with "Hey bot!" or "Bye")
-
-## Exercises
-
-Take a look at the `server.js` in this folder in VSCode or chosen editor.  Follow along in the code, commenting and uncommenting sections, to create different bot experiences.
-
-Have some fun!
-
-
-
+[here](/Instructor-Resources/BOTs/Node/bot-cardbot-instructor)
--- server.js
+++ server.js
@@ -1,134 +1,79 @@
 /*-----------------------------------------------------------------------------
-This Bot demonstrates basic bot setup, a simple hello dialog and a waterfall.
-There's also a trigger action included for fun.
+This Bot demonstrates basic bot setup, beginning and ending dialogs,
+the use of attachments and a confirmation prompt.
 
 It's part of the bot-education labs.
 
-Try this out with a test dialog ("hi", "bye" or any words really).
-Does it work as expected?  Does it get stuck anywhere?  Any changes needed?
+Try this out with a test dialog ("hi" is a good start).
 
-Make sure to try uncommenting out the waterfall part below.  Good to test this
-with the Bot Framework emulator (https://docs.botframework.com/en-us/tools/bot-framework-emulator/).
+Fill in the missing pieces below.
 
+There's a link to the docs you may need at the bottom.
 -----------------------------------------------------------------------------*/
 
-// Required packages
 var builder = require('botbuilder');
 var restify = require('restify');
 
-//============================================================
-// Setting up server and connector
-//============================================================
-
-// Connector options
-var botConnectorOptions = {
-    appId: process.env.MICROSOFT_APP_ID || "",
-    appPassword: process.env.MICROSOFT_APP_PASSWORD || ""
-};
-
-// Handle Bot Framework messages with a restify server
+// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   //When testing on a local machine, 3978 indicates the port to test on
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-// Instatiate the chat connector to route messages and create chat bot
-var connector = new builder.ChatConnector(botConnectorOptions);
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-//============================================================
-// Defining how bot carries on the conversation with the user
-//============================================================
-
-// Create our bot
-var bot = new builder.UniversalBot(connector);
-
-// This is the root dialog
-bot.dialog('/', [ 
-    function(session) {
-
-        // This is considered the root dialog ("/") and we can send a welcome here
-        session.send("Hi there!  Welcome to the root dialog.  You are going places...");
-
-        // "Push" the hello dialog onto the dialog stack
-        session.beginDialog('/hello');
-
-        // Uncomment to have the option of the waterfall dialog below, and comment the hello out
-        // It doesn't do much, but the code is interesting...
-        // session.beginDialog('/waterfall');
-    },
-    function(session, results) {
-        // Using the returned results, print out the response to the prompt and send reply
-        session.send("Welcome back to the root dialog, %s!", results.response);
-    }
-]);
-
-bot.dialog('/hello', [
-    function(session) {
-        session.send("Welcome to the hello dialog...")
-        builder.Prompts.text(session, "Hi there.  What's a good nickname for you?");
-    },
-    function(session, results) {
-        // "Pop" the current dialog off the stack and return to parent
-        session.endDialogWithResult(results);
-    }
-]);
-
-
-// This is a dialog with a waterfall (an array of functions for a conversation)
-// It makes use of a storage "bag" called the dialogData used for temp storage
-bot.dialog('/waterfall', [
-    function(session, args, next) {
-        //session.send("This is step one.");
-        session.dialogData.stepone = "Made it through step one.  ";
-        next();
-    },
-    function (session, results, next) {
-        session.dialogData.steptwo = "Made it through step two.  ";
-        next();
+// Dialog with small waterfall with some "routes"
+// These dialogs simply flow one after the other
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/picture');
     },
     function (session, results) {
-        var mymsg = session.dialogData.stepone + "<br>" + session.dialogData.steptwo + "<br>";
-        session.endDialog(mymsg + "Let's end this waterfall and return control to the root dialog :) .");
+        session.beginDialog('/cards');
     }
 ]);
 
-//============================================================
-// Set up some trigger actions
-//============================================================
+// Send a picture attachment from bot to user showing the use of a built-in Prompt
+bot.dialog('/picture', [
+    function (session) {
+        session.send("I can easily send pictures to a user...");
+        builder.Prompts.confirm(session, "Are you sure you want a famous robot picture sent?");
+    },
+    function (session, results) {
+        if (results.response) {
+        var msg = new builder.Message(session)
+        .text("You said yes")
+            
+            //...fill in with an image attachment that you like...
 
-// Example of a triggered action - when user types something matched by
-// the trigger, this dialog begins, clearing the stack and interrupting
-// the current dialog (so be cognizant of this).
-// What if we had put 'send' instead of 'endDialog' here - try this.
-bot.dialog('/bye', function (session) {
-    // end dialog with a cleared stack.  we may want to add an 'onInterrupted'
-    // handler to this dialog to keep the state of the current
-    // conversation by doing something with the dialog stack
-    session.endDialog("Ok... See you later.");
-}).triggerAction({matches: /^bye|Bye/i});
+        session.endDialog(msg);
+        } else {
+            var msg = new builder.Message(session)
+        .text("You said no");
+            session.endDialog(msg);
+        }
+    }
+]);
 
-//============================================================
-// Additional Exercises
-//============================================================
+// Send card attachment from bot to user, no waterfall here
+bot.dialog('/cards', [
+    function (session) {
+        session.send("I can easily send cards to a user...");
+        var msg = new builder.Message(session)
+            .text("Here's a card:")
+            .textFormat(builder.TextFormat.xml)
 
-// Add a user prompt for a fun user name or nickname
-// e.g.  builder.Prompts.text(session, 'Hi! What is your nickname?');
-// Where would this go and how would you include it?
-// Good ref:  https://docs.botframework.com/en-us/node/builder/chat/session/#starting-and-ending-dialogs
+            //...fill in with a hero card...
 
-//============================================================
-// Add-ons
-//============================================================
+        session.endDialog(msg);
+    }
+]);
 
-// Serve a static web page - for testing deployment (note: this is optional)
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
-
-
-
-// More samples here:  https://github.com/Microsoft/BotBuilder-Samples
-// And of course here:  https://github.com/Microsoft/BotBuilder/tree/master/Node/examples
+// Hint:  the reference for these are at
+// https://docs.botframework.com/en-us/node/builder/chat/session/#navtitle
